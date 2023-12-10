1765dc5e70ee4ccb5a5fabd3ceca3285a0858544
Chris Barry <chrisdavebarry@gmail.com>
2023-12-09 21:25:17 -0600

    Updated home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,23 @@
+// Home.jsx
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import './styles/Home.css';
 
 export default function Home() {
   return (
-    <Container>
-      <Row>
+    <Container className="text-center-vh">
+      <Row className="custom-row">
         <Col xs={12} md={8}>
-          <div className="text-center">
-            <h1>
-              cbarry@localhost:~<span className="blinking-cursor"></span>
-            </h1>
-          </div>
+          <h1>cbarry@localhost:~<span className="blinking-cursor"></span></h1>
         </Col>
+        <div className="logo-container">
+          <a href="https://github.com/chrisdavebarry/">
+            <img src="/img/github.png" alt="Github" className="logo"/>
+          </a>
+          <a href="https://www.linkedin.com/in/chrisdavebarry/">
+            <img src="/img/linkedin.png" alt="LinkedIn" className="logo"/>
+          </a>
+        </div>
       </Row>
     </Container>
   );
